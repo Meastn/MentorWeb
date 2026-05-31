66d15d84556bab723999281eb1f6d25453d1c939
mrcalikoglu <av.melihcalikoglu@protonmail.com>
2026-05-31 15:51:02 +0300

    Branding: Update official domain to www.mentorgroup.com.tr and coordinate emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // REF: MG-2026-ASTRO-CONFIG-01
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mentor.group',
+  site: 'https://www.mentorgroup.com.tr',
   i18n: {
     defaultLocale: 'tr',
     locales: ['tr', 'en'],
